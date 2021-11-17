de91d07a40731356ecf23dc66bd9c216a4f2e836
iskhakov-s <iskhakov.samuel@gmail.com>
2021-11-17 11:12:25 -0500

    added str and len to stack class, changed docstrings to reST

--- stacks/stack.py
+++ stacks/stack.py
@@ -1,80 +1,116 @@
-from  __future__ import annotations
+from __future__ import annotations
 from typing import Any, Union, Callable
 
 
 class Stack:
-    def __init__(self, *items: Any) -> None:
-        """[initializes a list, which will store the stack, and pushes any items given]
-        """             
+    def __init__(self, *items: Any, cap: int = None) -> None:
+        """
+        initializes a list, which will store the stack, and pushes any items given
+
+        :param items: optional items to be pushed to the stack
+        :param int cap: optional integer limiting size of stack
+        """
         self.lst = []
+        self.cap = cap
         self.push(*items)
-        
+
+    def __str__(self):
+        """
+        Returns stack items from left to right, equivalent to bottom to top
+
+        :return: stack as a str
+        """
+        return str(self.lst)
+
+    def __len__(self):
+        """
+        Returns length of the stack
+        Equivalent to stack.size
+
+        :rtype: int
+        :return: length
+        """
+        return self.size
+
     def push(self, *items: Any) -> None:
-        """[pushes items to the stack, from left to right]
-        """                       
+        """
+        Adds items to the top of the stack
+
+        :exception IndexError: if pushing item would exceed the cap
+        :param items: to be added
+        """
         for i in items:
+            if self.cap is not None and self.size >= self.cap:
+                raise IndexError(f'item could not be added, cap limit {self.cap} reached')
             self.lst.append(i)
         
-    def pop(self) -> Any:
-        """[removes the top item from the stack]
+    def pop(self, num=1) -> Union[Any, list[Any]]:
+        """
+        Removes the top item from the stack
 
-        Returns:
-            Any: [the top item of the stack]
-        """        
-        return self.lst.pop()
+        :exception IndexError: if stack has no items left
+        :param int num: number of times to pop
+        :return: the top item, or num top items from top to bottom in a list
+        """
+        if num == 1:
+            try:
+                self.lst.pop()
+            except IndexError:
+                raise IndexError('pop from empty stack')
+
+        pop_list = []
+        for i in range(num):
+            pop_list.append(self.pop())
+        return pop_list
         
     def peek(self) -> Any:
-        """[reveals, but does not remove, the top item of the stack]
-
-        Returns:
-            Any: [the top item of the stack]
+        """
+        Returns the top item from the stack without modification
         """
         return self.lst[-1]
-    
+
     @property
     def size(self) -> int:
-        """[returns the number of items in the list]
+        """
+        Returns the length of the stack
 
-        Returns:
-            int: [the length of the list]
-        """        
+        :rtype: int
+        """
         return len(self.lst)
     
     def is_empty(self) -> bool:
-        """[determines if list is empty]
+        """
+        Determines if the stack is empty
 
-        Returns:
-            bool: [True if empty else False]
-        """        
+        :rtype: bool
+        """
         return True if self.size == 0 else False
         
     @classmethod
     def reverse(cls, stack: Stack):
-        """[destroys a stack, and returns its elements in reverse order]
+        """
+        Returns a stack with elements in reverse
+        Original stack will be destroyed
 
-        Args:
-            stack (Stack): [the stack to be reversed]
-
-        Returns:
-            [type]: [the reversed stack]
-        """        
+        :param Stack stack: the stack
+        :rtype: Stack
+        """
         flipped = cls()
         for _ in range(stack.size):
             flipped.push(stack.pop())
         return flipped
     
     @classmethod
-    def pairs_match(cls, stack: Stack, left: str = '(', right: str = ')') -> bool:
-        """[checks if each right item corresponds to a left item in pairs]
+    def pairs_match(cls, stack: Stack, left: Any = '(', right: Any = ')') -> bool:
+        """
+        Checks if each right item corresponds to a left item in pairs
+        Stack will be destroyed
 
-        Args:
-            stack (Stack): [the stack to check for matching]
-            left (str, optional): [the left char to be matched]. Defaults to '('.
-            right (str, optional): [the right char to be matched]. Defaults to ')'.
-
-        Returns:
-            bool: [True if the Items come in pairs, else False]
-        """        
+        :param stack: the stack
+        :param left: The left item to match, defaults to '('
+        :param right: The right item to match, defaults to ')'
+        :return: bool
+        """
         unmatched_pairs = 0
         for _ in range(stack.size):
             if (x := stack.pop()) == right:
@@ -82,7 +118,7 @@ class Stack:
             elif x == left:
                 unmatched_pairs -= 1
             if unmatched_pairs < 0:
-                    return False
+                return False
         if unmatched_pairs != 0:
             return False
         return True
@@ -90,6 +126,16 @@ class Stack:
 
 # TODO - allow string operators instead of calling the function versions
 def postfix(*items: Union[float, Callable[[float, float], float]]) -> float:
+    """
+    Applies simple math expressions in postfix using stacks
+    Only supports functions, not math operators like +-*/
+    See operator module for word versions of some math operators
+
+    :exception ValueError: if the argument is not a number or function
+    :param items: numbers and functions using postfix notation
+    :type items: float, function
+    :rtype: float
+    """
     """[applies simple math expressions in postfix using stacks]
 
     See operator module for word versions of some math operators
