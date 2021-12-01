dae8eeb85c1f8a4e4fc834ba9fde07bcd4874d29
iskhakov-s <79172729+iskhakov-s@users.noreply.github.com>
2021-12-01 09:53:57 -0500

    implemented faster size property

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 from typing import Any
 
 
+SENTINEL = object()
+
+
 class Link:
     def __init__(self, val: Any = None, next_: Link = None):
         self.val = val
@@ -12,19 +15,23 @@ class Link:
 
 
 class LinkedList:
-    def __init__(self, first_item: Any = None) -> None:
-        self.head = Link(first_item) if first_item is not None else None
+    def __init__(self, first_item: Any = SENTINEL) -> None:
+        self.head = None
+        self.size = 0
+        if first_item is not SENTINEL:
+            self.append(first_item)
     
     def __len__(self) -> int:
         return self.size
     
+    def is_empty(self) -> bool:
+        return self.head is None
+    
     def __str__(self) -> str:
-        if self.size == 0:
-            return 'LinkedList[]'
-        string = 'LinkedList['
+        string = 'LinkedList[ '
         for link in self:
             string += str(link) + ','
-        return string[:-1] + ']'
+        return string[:-1] + ' ]'
     
     def __getitem__(self, index: int) -> Link:
         for count, link in enumerate(self):
@@ -52,9 +59,11 @@ class LinkedList:
     def append(self, item: Any) -> None:
         if self.size == 0:
             self.head = Link(item)
+            self.size += 1
             return
         new_link = Link(item)
         self.last().next_link = new_link
+        self.size += 1
         
     def index(self, item: Any) -> int:
         for idx,i in enumerate(self):
@@ -65,19 +74,24 @@ class LinkedList:
     def insert(self, index: int, item: Any) -> None:
         next_link = self[index]
         link = Link(item, next_link)
-        if index != 0:
+        if index == 0:
+            self.head = link
+        else:
             prev_link = self[index-1]
             prev_link.next_link = link
+        self.size += 1
     
     def remove(self, item: Any) -> int:
         if self.head.val == item:
             self.head = self.head.next_link
+            self.size -= 1
             return 0
         for idx, link in enumerate(self):
             if isinstance(link.next_link, Link) and link.next_link.val == item:
                 new_next = link.next_link.next_link
                 link.next_link = new_next
-                return idx
+                self.size -= 1
+                return idx+1
         return -1
         
     def pop(self, index: int) -> Any:
@@ -89,24 +103,26 @@ class LinkedList:
         val = prev_link.next_link.val
         new_next = prev_link.next_link.next_link
         prev_link.next_link = new_next
+        self.size -= 1
         return val
 
     @property
     def size(self) -> int:
-        count = 0
-        for _ in self:
-            count += 1
-        return count
+        return self._size
+        
+    @size.setter
+    def size(self, val: int) -> None:
+        self._size = val
         
 
 def main():
     ll = LinkedList()
-    print(ll)
+    print(ll.is_empty())
     for i in (0,34543,'dsfgd',"sdf"):
         ll.append(i)
-    print(ll)
-    print(ll.remove('dsfgd'))
-    print(ll)
+    print(len(ll))
+    print(ll.pop(1))
+    print(ll.is_empty())
 
 
 if __name__ == '__main__':
