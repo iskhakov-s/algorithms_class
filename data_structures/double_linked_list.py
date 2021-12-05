f8f189f20ad67fe6dadcc2cc2f04c7f5047f3d11
iskhakov-s <iskhakov.samuel@gmail.com>
2021-12-04 21:09:40 -0500

    fixed dll issues

--- data_structures/double_linked_list.py
+++ data_structures/double_linked_list.py
@@ -39,7 +39,6 @@ class DoubleLinkedList:
                 return link
         raise ValueError('index out of range')
 
-    # TODO test
     def __len__(self) -> int:
         return self.size
 
@@ -97,7 +96,6 @@ class DoubleLinkedList:
         old_tail.chain(self.tail)
         self.size += 1
 
-    # TODO test
     def pop(self, index: int = None) -> DoubleLink:
         if self.is_empty():
             raise ValueError('list is empty')
@@ -113,28 +111,35 @@ class DoubleLinkedList:
         if index == 0:
             rem_head = self.head
             self.head = self.head.next
+            self.head.prev = None
+            self.size -= 1
             return rem_head
         if index == self.size-1:
             rem_tail = self.tail
             self.tail = self.tail.prev
+            self.tail.next = None
+            self.size -= 1
             return rem_tail
         link = self[index]
         link.prev.chain(link.next)
         self.size -= 1
         return link
 
-    # TODO test
     def remove(self, item: Any, iter_forward=True) -> int:
-        if self.size == 1 and self.head.val == item:
-            self.head = self.tail = None
-            self.size -= 1
-            return 0
+        if self.size == 1:
+            if self.head.val == item:
+                self.head = self.tail = None
+                self.size -= 1
+                return 0
+            return -1
         if self.head.val == item:
             self.head = self.head.next
+            self.head.prev = None
             self.size -= 1
             return 0
         if self.tail.val == item:
-            self.tail = self.tail.next
+            self.tail = self.tail.prev
+            self.tail.next = None
             self.size -= 1
             return self.size
 
@@ -150,7 +155,6 @@ class DoubleLinkedList:
                 return idx
         return -1
 
-    # TODO test
     def index(self, item: Any, iter_forward=True) -> int:
         if iter_forward:
             link_iter = self.iter_fwd()
@@ -161,7 +165,6 @@ class DoubleLinkedList:
                 return idx
         return -1
 
-    # TODO test
     def insert(self, index: int, item: Any) -> None:
         new_link = DoubleLink(item)
         if index == 0:
@@ -169,22 +172,23 @@ class DoubleLinkedList:
             self.head = new_link
             self.size += 1
             return
-        if index == self.size-1:
+        if index == self.size:
             self.tail.chain(new_link)
             self.tail = new_link
             self.size += 1
             return
         old_link = self[index]
         old_link.prev.chain(new_link, old_link)
+        self.size += 1
 
 
 def main():
     dll = DoubleLinkedList()
     for i in range(10):
         dll.append_tail(i*2)
-    print(dll)
-    print(dll.pop())
-    print(dll)
+    print(dll, dll.size)
+    print(dll.insert(7, 5))
+    print(dll, dll.size)
 
 
 if __name__ == '__main__':
