be890b2bf9c76e52253d7ac3e5a92989aa17b3c0
iskhakov-s <79172729+iskhakov-s@users.noreply.github.com>
2021-12-21 10:03:28 -0500

    finished table1 hw

--- dynamic_programming/tables_1.py
+++ dynamic_programming/tables_1.py
@@ -27,4 +27,33 @@ class Sum134:
         
         
 def sticker_max(tup):
-    pass
+    vals = {tup[:1]:tup[0], tup[:2]:max(tup[:2]), tup[:3]:tup[0]+tup[2]}
+    for i in range(3, len(tup)):
+        vals[tup[:i+1]] = max(vals[tup[:i]], vals[tup[:i-1]] + tup[i])
+    return vals[tup]
+
+
+def main():
+    santa = Santa()
+    sum134 = Sum134()
+
+    print(santa(0)) # should be 1
+    print(santa(1)) # should be 0
+    print(santa(2)) # should be 1
+    print(santa(3)) # should be 2
+    print(santa(9)) # should be 133496
+
+    print(sum134(0)) # should be 1
+    print(sum134(1)) # should be 1
+    print(sum134(2)) # should be 1
+    print(sum134(3)) # should be 2
+    print(sum134(4)) # should be 4
+    print(sum134(8)) # should be 25
+
+    print(sticker_max((6, 1, 1, 3))) # should be 9
+    print(sticker_max((4, 1, 2, 10, 9))) # should be 15
+    print(sticker_max((1, 2, 3, 4))) # should be 6
+
+
+if __name__ == '__main__':
+    main()
