f79b4593829a92963fbbf476f41c5a8ee53a5e0a
iskhakov-s <79172729+iskhakov-s@users.noreply.github.com>
2022-02-02 10:14:42 -0500

    searching algorithms hw

---
+++ search/searching_algorithms.py
@@ -0,0 +1,51 @@
+class Student:
+    def __init__(self, name, id=0):
+        self.name = name
+        self.id = id
+    def __str__(self):
+        return f'Student: {self.name}, {self.id}'
+
+def linear_search(arr, keyid):
+    """
+    arr - list of Students
+    keyid - int
+    
+    Returns the index of the Student in arr whose id matches keyid, -1 if not found
+    """
+    
+    for i, student in enumerate(arr):
+        if student.id == keyid:
+            return i
+    return -1
+
+def binary_search(arr, keyid):
+    """
+    arr - list of Students, sorted in increasing order by id
+    keyid - int
+    
+    Returns the index of the Student in arr whose id matches keyid, -1 if not found
+    """
+    low = 0
+    high = len(arr)-1
+    while True:
+        if low > high:
+            break
+        mid = (high+low)//2
+        id_ = arr[mid].id
+        if id_ == keyid:
+            return mid
+        elif id_ < keyid:
+            low = mid + 1
+        else:
+            high = mid - 1
+        
+    
+    return -1
+
+lst = [Student(name, id**2) for id, name in enumerate("ABCDEFG")]
+#uncomment to see students:
+#for s in lst: print(s)
+print(linear_search(lst, 3)) #should be -1
+print(linear_search(lst, 4)) #should be 2
+print(binary_search(lst, 3)) #should be -1
+print(binary_search(lst, 4)) #should be 2
