d7ad2d78e0a07dc25e7e6a94fe999a8b3880eed3
SAMUEL ISKHAKOV <ISKHAKOVS>
2022-02-10 15:00:08 -0500

    added delete method

--- hash/hash.py
+++ hash/hash.py
@@ -1,7 +1,9 @@
-from typing import Callable
+import logging
 
 
 class HashTable:
+    DEL = 'deleted'
+    
     def __init__(self, cap=13):
         self.arr = [None]*cap
         self.cap = cap
@@ -13,10 +15,11 @@ class HashTable:
             # number theory guarantees that indexes will
             # start repeating after half of them are tried
             idx = self.h(key, i)
-            if self.arr[idx] is None:
+            if self.arr[idx] is None or self.arr[idx] == self.DEL:
                 self.arr[idx] = key
-                return f"{key} was inserted in position {idx}"
-        return f"{key} was not inserted"
+                logging.info(f"{key} was inserted in position {idx}")
+                return 
+        logging.info(f"{key} was not inserted")
 
     def find(self, key):
         idx = None
@@ -25,18 +28,34 @@ class HashTable:
             idx = self.h(key, i)
             i += 1
         if self.arr[idx] == key:
+            logging.info(f'{key} found in position {idx}')
             return idx
+        logging.info(f'{key} not found')
         return -1
-    
+
+    def delete(self, key):
+        idx = self.find(key)
+        if idx > 0:
+            logging.info(f'{key} deleted from position {idx}')
+            self.arr[idx] = self.DEL
+        logging.info(f'{key} not deleted')
 
 
-def hash_idx(func: Callable, key: int, num_iter: int):
-    """[Returns a list of the first num_iter indices checked for a given hash]
+def hash_idx(func, key, n):
+    """[Returns a list of the first n indices checked for a given hash function and key]
     """    
-    return [func(key, i) for i in range(num_iter)]
+    return [func(key, i) for i in range(n)]
 
 
-
-tbl = HashTable()
-for key in [81, 22, 32, 49, 29, 74]:
-    print(tbl.insert(key))
+def main():
+    logging.basicConfig(level=logging.INFO)  
+    tbl = HashTable()
+    for key in [81, 22, 32, 49, 29, 74]:
+        tbl.insert(key)
+    tbl.find(81)
+    tbl.find(123432434)
+    tbl.delete(81)
+    tbl.delete(123432434)
+    
+if __name__ == '__main__':
+    main()
