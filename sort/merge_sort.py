c07138336e7727ea8821ad4f0568d356c4a18cca
SAMUEL ISKHAKOV <ISKHAKOVS>
2022-02-16 10:15:42 -0500

    merge sort

---
+++ sort/merge_sort.py
@@ -0,0 +1,39 @@
+import logging
+
+
+def merge_sort(arr, first=True):
+    logging.info(str(arr))
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        merge_sort(left:= arr[:mid], False)
+        merge_sort(right := arr[mid:], False)
+        idx_l = idx_r = idx = 0
+        while len(left) > idx_l and len(right) > idx_r:
+            if left[idx_l] > right[idx_r]:
+                arr[idx] = right[idx_r]
+                idx_r += 1
+            else:
+                arr[idx] = left[idx_l]
+                idx_l += 1
+            idx += 1
+        while idx_l < len(left):
+            arr[idx] = left[idx_l]
+            idx += 1
+            idx_l += 1
+        while idx_r < len(right):
+            arr[idx] = right[idx_r]
+            idx += 1
+            idx_r += 1
+    if first:
+        return arr
+
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    lst =  [314, 159, 265, 358, 979, 323, 846, 264, 338, 327, 950, 288, 419, 716]
+    print(merge_sort(lst))
+
+
+if __name__ == '__main__':
+    main()
