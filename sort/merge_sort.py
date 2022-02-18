ca691d5ebc384bf2f2beb76b7ab761f8782e07d6
iskhakov-s <iskhakov.samuel@gmail.com>
2022-02-17 23:15:34 -0500

    began implementing in place merge sort

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -28,9 +28,36 @@ def merge_sort(arr, first=True):
         return arr
 
 
-def merge_sort2(arr, l, r):
-    
-    pass
+def merge_sort2(lst, aux=None, lo=0, hi=None):
+    if aux is None:
+        aux = [0]*len(lst)
+    if hi is None:
+        hi = len(lst)
+    mid = (lo + hi) // 2
+    # Write the base case.
+    if (hi-lo) <= 1:
+        return
+    print(hi)
+    # After the completing the base case, uncomment below
+    merge_sort2(lst, aux, lo, mid)
+    merge_sort2(lst, aux, mid, hi)
+    idxa = lst.index(0) # Use the correct initial value of idxa, the index for aux.
+    idx1 = lo # Use the correct initial value of idx1, the index for the first half.
+    idx2 = mid # Use the correct initial value of idx2, the index for the second half.
+    while idx1 < mid or idx2 < hi:
+        # "base case": What are the conditions for finishing one of the halves?
+        
+        # How do you fill in aux?
+        if lst[idx2] < lst[idx1] or idx1 >= mid:
+            aux[idxa] = lst[idx2]
+            idx2 += 1
+        else:
+            aux[idxa] = lst[idx1]
+            idx1 += 1
+        idxa += 1
+    logging.info(str(lst), str(aux))
+    lst[:] = aux[:]
+
 
 
 def main():
